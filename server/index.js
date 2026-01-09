ed6a5200eb53dd29fcf4ad4de2043e9f22e74a65
rahulmandyal1 <rahulmandyal079@gmail.com>
2026-01-09 16:26:26 +0530

    fix:cors errors

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('apollo-server-express');
 const express = require('express');
+const cors = require('cors');
 const connectToDB = require('./db');
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
@@ -8,6 +9,16 @@ const { PORT } = require('./utils/config');
 connectToDB();
 
 const app = express();
+
+// Enable CORS for all routes
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://stack-underflow-frontend.onrender.com',
+    'https://stack-underflow-frontend.onrender.com/'
+  ],
+  credentials: true
+}));
 
 // Simple GET endpoint at root
 app.get('/', (req, res) => {
@@ -18,9 +29,16 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: ({ req }) => ({ req }),
+  cors: {
+    origin: [
+      'http://localhost:3000',
+      'https://stack-underflow-frontend.onrender.com'
+    ],
+    credentials: true
+  }
 });
 
-server.applyMiddleware({ app, path: '/graphql' });
+server.applyMiddleware({ app, path: '/graphql', cors: false });
 
 app.listen({ port: PORT }, () => {
   console.log(`Server ready at http://localhost:${PORT}`);
